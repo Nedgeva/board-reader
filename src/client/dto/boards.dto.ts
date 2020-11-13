e29dbe7cd71f6930c3147e68b2c8ee8aafdeddf6
Pavel Udalov <pavel.udalov@devexperts.com>
2020-11-13 23:22:35 +0400

    added validation for boards

--- src/client/dto/boards.dto.ts
+++ src/client/dto/boards.dto.ts
@@ -1,3 +1,5 @@
+import * as t from "io-ts";
+
 export interface Boards {
   boards?: BoardsEntity[] | null;
   global_boards: number;
@@ -25,3 +27,57 @@ export interface TagsEntity {
   board: string;
   tag: string;
 }
+
+const Boards = t.intersection([
+  t.type({
+    global_boards: t.number,
+    global_posts: t.string,
+    global_speed: t.string,
+    is_index: t.number,
+    type: t.number,
+  }),
+  t.partial({
+    boards: t.union([
+      t.undefined,
+      t.null,
+      t.array(
+        t.type({
+          bump_limit: t.number,
+          category: t.string,
+          default_name: t.string,
+          enable_names: t.number,
+          enable_sage: t.number,
+          id: t.string,
+          info: t.string,
+          last_num: t.number,
+          name: t.string,
+          speed: t.number,
+          threads: t.number,
+          unique_posters: t.number,
+        })
+      ),
+    ]),
+    tags: t.union([
+      t.undefined,
+      t.null,
+      t.array(t.type({ board: t.string, tag: t.string })),
+    ]),
+  }),
+]);
+
+const BoardsEntity = t.type({
+  bump_limit: t.number,
+  category: t.string,
+  default_name: t.string,
+  enable_names: t.number,
+  enable_sage: t.number,
+  id: t.string,
+  info: t.string,
+  last_num: t.number,
+  name: t.string,
+  speed: t.number,
+  threads: t.number,
+  unique_posters: t.number,
+});
+
+const TagsEntity = t.type({ board: t.string, tag: t.string });
